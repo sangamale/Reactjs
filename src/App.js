14995a1c0c967ac8ec71724d527ac7535bb49fbf
@sangamale <babuale7@gmail.com>
2021-03-18 15:25:52 +0545

    second commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import First from './First';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>hello world</h1>
+      <First/> */}
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
